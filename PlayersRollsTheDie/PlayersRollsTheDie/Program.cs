c58b8da886b1bbf273bfd8fedc8cd14b6f05fb92
Kundan <kundan kamble>
2022-05-13 06:42:46 -0700

    [KUNDAN] Added Rolls The Die

---
+++ PlayersRollsTheDie/PlayersRollsTheDie/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SnakesAndLadder
+{
+    internal class Program
+    {
+        public static int RollDice()
+        {
+            return new Random().Next(1, 7);
+        }
+        static void Main(string[] args)
+        {
+            int position = 0;
+            Console.WriteLine("Player is at {0}", position);
+            Console.WriteLine("Player gets: " + RollDice());
+        }
+    }
+}
+
