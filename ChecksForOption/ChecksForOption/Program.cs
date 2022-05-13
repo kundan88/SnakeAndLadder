8fef554763b1b8cbb045626b03c387c5a8b5b9e7
Kundan <kundan kamble>
2022-05-13 06:55:21 -0700

    [KUNDAN] Added Checks for option program

---
+++ ChecksForOption/ChecksForOption/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace SnakesAndLadder
+{
+    internal class Program
+    {
+        public static int RollDice()
+        {
+            return new Random().Next(1, 7);
+        }
+        static void Main(string[] args)
+        {
+            int position = 0;
+            int checkOption = new Random().Next(0, 3);
+            int rollDice = RollDice();
+            Console.WriteLine("PLayer got {0} on dice", rollDice);
+
+            switch (checkOption) 
+            {
+                case 0:
+                    position += 0;
+                    Console.WriteLine("Player stayed at {0} position", position);
+                    break;
+                case 1:
+                    position += rollDice;
+                    Console.WriteLine("Player moved to {0} position", position);
+                    break;
+                case 2:
+                    if (position >= rollDice)
+                    {
+                        position -= rollDice;
+                    }
+                    Console.WriteLine("Player moved back to {0} position", position);
+                    break;
+            }
+
+        }
+    }
+}
+
