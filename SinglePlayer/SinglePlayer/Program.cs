6b757ddac5c8c454449ebc4c47d51e9be4527b50
Kundan <kundan kamble>
2022-05-13 06:38:16 -0700

    [Kundan] Added Single Player Program

---
+++ SinglePlayer/SinglePlayer/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SnakesAndLadder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int position = 0;
+            Console.WriteLine("Player is at {0}", position);
+        }
+    }
+}
